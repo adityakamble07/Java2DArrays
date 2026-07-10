18bcdfa391da55a27f47a0946fe6cc4d3d27d684
adityakamble07 <adityakamble95482@gmail.com>
2026-07-10 20:55:23 +0530

    Print Main Diagonal Of The Matrix

--- src/Java2DArrays/PrintMainDiagonal.java
+++ src/Java2DArrays/PrintMainDiagonal.java
@@ -1,4 +1,25 @@
 package Java2DArrays;
-
+import java.util.*;
 public class PrintMainDiagonal {
-}
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size of rows=");
+        int rows=sc.nextInt();
+        System.out.println("enter the size of columns=");
+        int columns=sc.nextInt();
+        System.out.println("enter the Matrix "+"("+rows+"X"+columns+")");
+        int arr[][]=new int[rows][columns];
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<arr.length;i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (i==j) {
+                    System.out.println(arr[i][j]);
+
+                }
+
+            }
+        }}}
